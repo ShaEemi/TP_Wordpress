e87d1cf7a47e933c0d73908e27d2d78183316041
ShaEemi <Shash@MacBook-Air-de-Sharon.local>
2015-11-04 17:28:04 +0100

    ajout mon_plugin
    ajoute mon plugin de wordpress vers mon depot

---
+++ mon_plugin.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+
+	Plugin Name: Mon Plugin
+	Description: Mon 1er plugin
+	Version: 0.1
+
+*/
+
+
+defined('ABSPATH') or die('No script kiddies please !');
+
+add_filter('the_content','ma_fonction');
+function ma_fonction($content){
+	return str_replace('test', 'nimp', $content);
+}
+
+add_action( 'plugins_loaded', 'myplugin_load_textdomain' );
+/**
+ * Load plugin textdomain.
+ *
+ * @since 1.0.0
+ */
+function myplugin_load_textdomain() {
+  load_plugin_textdomain( 'my-plugin', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' ); 
+}
